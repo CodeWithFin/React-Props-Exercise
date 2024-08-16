ce0dcb350a57db407bec0c3cc333e51c09e152d6
Finley <finley.mwachia12@gmail.com>
2024-08-16 14:11:38 +0300

    project working successfully

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-
+import Card from "./Card";
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        contact="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        contact="+987 654 321"
+        email="jack@nowhere.com"
+      />
+
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        contact="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className ="name">{props.name}</h2>
+          <img src={props.img} alt="avatar_img" className = "circle-img"/>
+        </div>
+        <div className="bottom">
+          <p className="info">{props.contact}</p>
+          <p className="info"> {props.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
